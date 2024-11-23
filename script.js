6debeb871125f008b3eb1abcbc3e4be42f2e4680
kizmusnoel <kizmus.noel@students.jedlik.eu>
2024-11-23 11:19:46 +0100

    upload

---
+++ script.js
@@ -0,0 +1,132 @@
+const colors = ['red', 'blue', 'green', 'yellow'];
+const values = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13'];
+let IMAGES = [];
+
+
+function Start() {
+    const players = document.querySelectorAll('.hand');
+    const cards = document.querySelectorAll('.card');
+    for (let i = 0; i < 14; i++) {
+        for (let j = 0; j < 8; j++) {
+            IMAGES.push('' + i + j);
+        }
+    }
+
+
+    for (let index = 0; index < 7; index++) {
+        players.forEach((player, i) => {
+            player.appendChild(RandomCard("" + i + index));
+        });
+    }
+    document.querySelector('#startBtn').remove();
+}
+
+function RandomCard(index) {
+    let card = document.createElement('img');
+    let classValue = '';
+    while (true) {
+        color = colors[Math.floor(Math.random() * colors.length)];
+        value = values[Math.floor(Math.random() * values.length)];
+
+        let IMAGE = value;
+
+        switch (color) {
+            case 'red':
+                IMAGE += '0';
+                break;
+            case 'blue':
+                IMAGE += '3';
+                break;
+            case 'green':
+                IMAGE += '2';
+                break;
+            case 'yellow':
+                IMAGE += '1';
+                break;
+        }
+
+
+        if (IMAGES.includes(IMAGE)) {
+            card.src = 'img/cards/' + IMAGE + '.png';
+            for (let index = 0; index < IMAGES.length; index++) {
+                if (IMAGES[index] == IMAGE) {
+                    IMAGES.splice(index, 1);
+                    break;
+                }
+
+            }
+            break
+        }
+
+
+    }
+    switch (value) {
+        case '10':
+            classValue = 'skip';
+            break;
+        case '11':
+            classValue = 'reverse';
+            break;
+        case '12':
+            classValue = '+2';
+            break;
+        case '13':
+            classValue = 'wild';
+            break;
+        default:
+            classValue = value;
+            break;
+    }
+    card.id = "card" + index;
+    card.classList.add('card');
+    card.classList.add(classValue == "wild" ? classValue : color);
+    card.classList.add(classValue);
+    card.draggable = true;
+
+    return card;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+function nameDragStart(event) {
+    event.dataTransfer.setData('text', event.target.id);
+    console.log(event.target.id)
+}
+
+function groupDragOver(event) {
+    const group = event.target;
+    if (!group.classList.contains('hand')) return
+    group.style.border = '5px dashed black';
+    group.style.backgroundColor = '#ccc';
+
+
+
+    event.preventDefault();
+}
+
+function groupDragLeave(event) {
+
+    const group = event.target;
+    if (!group.classList.contains('hand')) return
+    group.style.border = '1px solid black';
+    group.style.backgroundColor = '#a3a3a3';
+}
+
+function drop(event) {
+    const group = event.target;
+    const card = document.getElementById(event.dataTransfer.getData('text'));
+    if (!group.classList.contains('hand')) return
+    console.log(group)
+    group.style.border = '1px solid black';
+    group.style.backgroundColor = '#a3a3a3';
+    group.appendChild(card);
+}
